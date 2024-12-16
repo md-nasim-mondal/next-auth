fe58df5d554a4d27c4777931fd7346d0ddbe8cda
md-nasim-mondal <nasim.programmer24@gmail.com>
2024-12-16 12:33:21 +0600

    made auth related api with different options file

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,38 @@
+import type { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Next Auth",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Enter your email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+          const user = {
+              id: "1",
+              name: "J Smith",
+              email: credentials?.email
+          };
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+  ],
+};
