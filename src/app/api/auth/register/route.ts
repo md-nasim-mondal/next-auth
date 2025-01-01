a18f6bfb732d1a11bff03d0f12bd59df4102cab5
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-01-01 17:39:38 +0600

    used vine.js packages for validating data and made authSchema for checking auth related data and also made a ErrorReporter file for customize Error Type

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,10 +1,22 @@
+import { registerSchema } from "./../../../../validator/authSchema";
 import { connect } from "@/database/mogo.config";
+import ErrorReporter from "@/validator/ErrorReporter";
+import vine, { errors } from "@vinejs/vine";
 import { NextResponse, type NextRequest } from "next/server";
 
 // For DB Connection
 connect();
 
-export async function POST(request:NextRequest) {
+export async function POST(request: NextRequest) {
+  try {
     const body = await request.json();
-    return NextResponse.json(body, {status: 200})
+      const validator = vine.compile(registerSchema);
+      validator.errorReporter = () => new ErrorReporter()
+    const output = await validator.validate(body);
+    return NextResponse.json(output, { status: 200 });
+  } catch (error) {
+    if (error instanceof errors?.E_VALIDATION_ERROR) {
+      return NextResponse.json(error?.messages, { status: 200 });
+    }
+  }
 }
