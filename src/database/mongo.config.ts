678c42abc73450fe749c591ff74627fbd446c378
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-01-03 21:02:59 +0600

    update code and try to fix suspense error

--- src/database/mongo.config.ts
+++ src/database/mongo.config.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export function connect() {
   mongoose
-    .connect(process.env.MONGO_URI!, {
+    .connect(process.env.MONGO_URI as string, {
       tls: true,
       ssl: true,
     })
