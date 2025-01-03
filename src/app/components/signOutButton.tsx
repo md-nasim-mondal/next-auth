00e2350fa0c23c2615e55c5d0233ee944b21c735
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-01-03 18:46:18 +0600

    implement logOut button

--- src/app/components/signOutButton.tsx
+++ src/app/components/signOutButton.tsx
@@ -1,10 +1,15 @@
 "use client";
+import { signOut } from "next-auth/react";
 import React from "react";
 
 export default function SignOutButton() {
   return (
     <div>
-      <button className='bg-orange-300 rounded-md p-2'>Signout</button>
+      <button
+        className='bg-orange-300 rounded-md p-2'
+        onClick={() => signOut({ callbackUrl: "/login", redirect: true })}>
+        Signout
+      </button>
     </div>
   );
 }
