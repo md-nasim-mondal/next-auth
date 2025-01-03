3d6fef86b98ae003fe1226749b7a6e5aa914622b
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-01-03 22:08:18 +0600

    fixed login page not showing problem with NextConfig code changed added base path

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-  basePath: '/',
+  basePath: '/myapp',
   i18n: {
     locales: ['en', 'bn'],
     defaultLocale: 'en',
