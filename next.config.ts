4af8b9c378bd71c62da98287c6f90800695a1731
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-01-04 00:04:36 +0600

    solved turbo problem

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,24 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['example.com'], // Replace with your image host domain
   },
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        underscore: 'lodash',
+        mocha: { browser: 'mocha/browser-entry.js' },
+      },
+      resolveExtensions: [
+        '.mdx',
+        '.tsx',
+        '.ts',
+        '.jsx',
+        '.js',
+        '.mjs',
+        '.json',
+      ],
+    },
+  },
+  
 };
 
 export default nextConfig;
