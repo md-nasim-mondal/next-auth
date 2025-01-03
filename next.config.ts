8c9f1f2d32e765c4da48c1c3f65596a8319935bf
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-01-04 01:50:23 +0600

    update next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-
+  /* config options here */
   images: {
     remotePatterns: [
       {
@@ -12,35 +12,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // reactStrictMode: true,
-  // pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-  // i18n: {
-  //   locales: ['en', 'bn'],
-  //   defaultLocale: 'en',
-  // },
-  // webpack(config) {
-  //   return config;
-  // },
-  // images: {
-  //   domains: ['example.com'], // Replace with your image host domain
-  // },
-  // experimental: {
-  //   turbo: {
-  //     resolveAlias: {
-  //       underscore: 'lodash',
-  //       mocha: { browser: 'mocha/browser-entry.js' },
-  //     },
-  //     resolveExtensions: [
-  //       '.mdx',
-  //       '.tsx',
-  //       '.ts',
-  //       '.jsx',
-  //       '.js',
-  //       '.mjs',
-  //       '.json',
-  //     ],
-  //   },
-  // },
   
 };
 
